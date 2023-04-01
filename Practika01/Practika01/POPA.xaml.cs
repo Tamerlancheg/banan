37074d86e9dcdc847d02c5871dd528cc80bbc271
Tamerlancheg <113166464+Tamerlancheg@users.noreply.github.com>
2023-04-01 15:15:37 +0300

    Add files via upload

---
+++ Practika01/Practika01/POPA.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Practika01.masterDataSetTableAdapters;
+
+namespace Practika01
+{
+    public partial class POPA : Window
+    {
+        saleTableAdapter sale = new saleTableAdapter();
+        public POPA()
+        {
+            InitializeComponent();
+            saleDataGrid.ItemsSource = sale.GetData();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PIPA wim = new PIPA();
+            wim.Show();
+        }
+    }
+}
